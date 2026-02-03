c216c73ba7cdb82bd98bc2cc1ad4f18dd8181e21
gillesruelens-collab <gilles.ruelens@gmail.com>
2026-02-03 15:44:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // De 'base' moet exact overeenkomen met je repository naam op GitHub
-  base: '/Eduhelper/',
+  // De 'base' MOET exact je repository naam zijn met slashes
+  base: '/Eduhelper/', 
   plugins: [react()],
   build: {
-    // Dit zorgt ervoor dat de build-output in de juiste map terechtkomt voor de Action
     outDir: 'dist',
   }
 })
